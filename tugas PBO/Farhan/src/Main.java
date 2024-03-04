b1eae92aff4bb40b87aeae00539b0a95c00673b1
farhann300303 <145762749+farhann300303@users.noreply.github.com>
2024-03-04 14:41:14 +0700

    Add files via upload

---
+++ tugas PBO/Farhan/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Kucing Kucing = new Kucing();
+        Kucing.nama = "Amel";
+        Kucing.age = 20;
+        Kucing.address = "Banyuwangi";
+        Kucing.Jenis = "Omnifora";
+        Kucing.Kel = "Perempuan";
+        Kucing.Pisang();
+    }
+}
